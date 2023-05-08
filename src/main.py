710ad27b2f18ab5e39eb22fe88c586e97da30070
Razvan Savu <razzvan.savu@gmail.com>
2023-05-09 00:11:12 +0300

    Add Todoist client

--- src/main.py
+++ src/main.py
@@ -1,5 +1,33 @@
 import argparse
 import sqlite3 as sl
+import requests
+import json
+
+class Task:
+    def __init__(self, id):
+        self.id = id
+    def set_content(self, content):
+        self.content = content
+    def __str__(self) -> str:
+        return f'[id:{self.id}] [content:{self.content}]'
+
+class TodoistClient:
+    def __init__(self, key):
+        self.key = key
+    
+    def get_tasks(self):
+        url = 'https://api.todoist.com/rest/v2/tasks'
+        headers = {'Authorization': 'Bearer ' + self.key}
+        res = requests.get(url, headers=headers)
+        if (res.status_code == 401):
+            print('Forbidden')
+            return
+        ret = []
+        for item in res.json():
+            task = Task(item['id'])
+            task.set_content(item['content'])
+            ret.append(task)
+        return ret
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog = 'Organizer')
@@ -7,4 +35,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     con = sl.connect('test.db')
+
+    todoist_client = TodoistClient(args.key)
+    tasks = todoist_client.get_tasks()
+    print(tasks[0])
     
