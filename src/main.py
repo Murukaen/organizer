4a372efc7f9feb4ca03d0eadb50d14a5443431f7
razvan <razzvan.savu@gmail.com>
2023-05-20 12:38:01 +0000

    Call Todoist sync API

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import argparse
 import sqlite3 as sl
 import requests
+import json
 
 class Task:
     def __init__(self, id):
@@ -28,6 +29,24 @@ class TodoistClient:
             ret.append(task)
         return ret
 
+    def get_tasks_sync(self):
+        url = 'https://api.todoist.com/sync/v9/sync'
+        headers = {'Authorization': 'Bearer ' + self.key}
+        payload = {
+            'sync_token': '*',
+            'resource_types': '["items"]'
+        }
+        res = requests.post(url, headers=headers, json=payload)
+        if (res.status_code == 401):
+            print('Forbidden')
+            return
+        ret = []
+        for item in res.json()['items']:
+            task = Task(item['id'])
+            task.set_content(item['content'])
+            ret.append(task)
+        return ret
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog = 'Organizer')
     parser.add_argument('-k', '--key', required = True)
@@ -37,7 +56,7 @@ if __name__ == '__main__':
     cur = con.cursor()
 
     todoist_client = TodoistClient(args.key)
-    tasks = todoist_client.get_tasks()
+    tasks = todoist_client.get_tasks_sync()
 
     for task in tasks:
         content = task.content.replace('\'', '\'\'')
