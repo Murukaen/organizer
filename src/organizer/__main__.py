3668b9415b16b8b3df0c0e780fd0494e183da2fd
razvan <razzvan.savu@gmail.com>
2024-10-12 16:21:56 +0300

    Refactor CLI

--- src/organizer/__main__.py
+++ src/organizer/__main__.py
@@ -7,7 +7,7 @@ def parse_args_and_execute():
     parser = argparse.ArgumentParser(prog = 'Organizer')
     parser.add_argument('-k', '--key', required = True)
     parser.add_argument('-d', '--database', required = True)
-    sub_parsers = parser.add_subparsers(help='sub_parsers help here', dest='subparser_name', required=True)
+    sub_parsers = parser.add_subparsers(help='sub_parsers help here', dest='command', required=True)
     parser_clear = sub_parsers.add_parser('clear', help='clear db')
     parser_sync = sub_parsers.add_parser('sync', help='desc here')
     parser_search = sub_parsers.add_parser('search', help='desc here')
@@ -18,11 +18,11 @@ def parse_args_and_execute():
     
     organizer = Organizer(args.database, args.key)
     
-    if args.subparser_name == 'clear':
+    if args.command == 'clear':
         organizer.clear_db()
-    elif args.subparser_name == 'sync':
+    elif args.command == 'sync':
         organizer.sync()
-    elif args.subparser_name == 'search':
+    elif args.command == 'search':
         if args.query:
             organizer.search(args.query)
         else:
